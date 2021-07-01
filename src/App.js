281fa5f0249a7ec6cfac856e1f3c7592ee3164c6
Maykel Nekel <maykelnekel@gmail.com>
2021-07-01 19:12:00 -0300

    Repositório iniciado, pasta componentes e seus filhos criados, inicio da l

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import MenuContainer from './Components/MenuContainer'
 
 function App() {
+  const [products, setProducts] = useState([
+    { id: 1, name: 'Hamburguer', category: 'Sanduíches', price: 7.99 },
+    { id: 2, name: 'X-Burguer', category: 'Sanduíches', price: 8.99 },
+    { id: 3, name: 'X-Salada', category: 'Sanduíches', price: 10.99 },
+    { id: 4, name: 'Big Kenzie', category: 'Sanduíches', price: 16.99 },
+    { id: 5, name: 'Guaraná', category: 'Bebidas', price: 4.99 },
+    { id: 6, name: 'Coca', category: 'Bebidas', price: 4.99 },
+    { id: 7, name: 'Fanta', category: 'Bebidas', price: 4.99 },
+  ]); 
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [currentSale, setCurrentSale] = useState({total: 0, saleDetails: []})
+
+  const showProducts = () => {
+
+  }
+  const handleClick = (productId) => {
+      
+  }
+
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hamburgueria da Kenzie</h1>
       </header>
-    </div>
+      <section className='App-menuContainer'>
+        <div>
+          <MenuContainer products = {products} filteredProducts = {filteredProducts} currentSale = {currentSale} />
+        </div>
+        <div className= 'App-totalPrice'>
+            {currentSale.reduce()}
+        </div>
+      </section>
+    </main>
   );
 }
 
---
+++ src/Components/MenuContainer/index.jsx
@@ -0,0 +1,22 @@
+import Product from '../Product'
+
+
+function MenuContainer({ products, filteredProducts, currentSale }) {
+
+
+    return (
+        <section className='products'>
+               
+            <div>
+                { products.map((item, index) => 
+                <div>
+                    <Product key = {index} products = {item} />
+                </div>
+                
+                )}
+            </div>
+        </section>
+    )
+}
+
+export default MenuContainer
